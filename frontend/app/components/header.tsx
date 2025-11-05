cc96a8cf37763464500ce7420fdb48da67b483c2
Muhammad Umar <uazam3033@gmail.com>
2025-11-05 13:16:16 -0800

    Initial commit for frontend

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 import { NAV_LINKS } from "../share/data";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,8 +8,25 @@ import Button from "@/app/components/button";
 import { FaCommentMedical } from "react-icons/fa";
 
 export default function Header() {
+  const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 50); 
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="fixed top-0 left-0 w-full z-50 bg-linear-to-r from-[#1E2433] via-[#3D2F78] to-[#1E2433] backdrop-blur-md border-b border-[#2B3142]">
+    <header
+      className={`fixed border-bottom border-gray-400 top-0 left-0 w-full z-50 border-b transition-all duration-300 ${
+        scrolled
+           ? "bg-linear-to-r from-[#1E2433]/90 via-[#3D2F78]/90 to-[#1E2433]/90 border-[#2B3142] backdrop-blur-md"
+          : "bg-transparent "
+      }`}
+    >
       <div className="max-w-7xl mx-auto flex items-center justify-between px-6 py-3">
         <div className="flex items-center h-[60px]">
           <div className="h-[60px] w-[60px] flex items-center justify-center">
@@ -21,9 +39,9 @@ export default function Header() {
               priority
             />
           </div>
-          <span className="font-bold text-lg text-white leading-none">
+          <span className="font-semibold text-xl text-white leading-none">
             Clarity&nbsp;
-            <span className="bg-linear-to-r from-[#a373f6] to-blue-500 bg-clip-text text-transparent">
+            <span className="bg-linear-to-r from-[#7C3AED] to-[#2563EB] bg-clip-text text-transparent">
               Cosmos
             </span>
           </span>
@@ -40,14 +58,13 @@ export default function Header() {
             </Link>
           ))}
         </nav>
-        <div>
+
+        <Link href="">
           <Button className="bg-[#7C3AED] hover:bg-[#6D28D9] text-white flex items-center gap-2 px-4 py-2 rounded-lg text-sm font-semibold transition-all duration-300 shadow-md">
-            <Link href="" className="flex items-center gap-2">
-              <FaCommentMedical className="w-3 h-3 text-white" />
-              <span>Try Beta Free</span>
-            </Link>
+            <FaCommentMedical className="w-3 h-3 text-white" />
+            <span>Try Beta Free</span>
           </Button>
-        </div>
+        </Link>
       </div>
     </header>
   );
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import Header from "./components/header";
+import Hero from "./components/landing/hero";
 
 export default function Home() {
   return (
-    <>
+    <main className="min-h-screen flex flex-col bg-[#111827]">
       <Header />
-    </>
+      <Hero />
+    </main>
   );
 }
